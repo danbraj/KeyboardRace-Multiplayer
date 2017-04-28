94e77271be87d2a984cc0683e27e0633ffd2dce4
danbraj <danbraj@users.noreply.github.com>
2017-04-28 15:04:00 +0200

    dodanie klasy ObslugaPlikow

---
+++ ObslugaPlikow.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+class ObslugaPlikow {
+
+    public static String getFileExtension(File file) {
+        String fileName = file.getName();
+        if (fileName.lastIndexOf(".") != -1 && fileName.lastIndexOf(".") != 0)
+            return fileName.substring(fileName.lastIndexOf(".") + 1);
+        else
+            return "";
+    }
+
+    public static void stringToFile(String fileName, String content) {
+
+        File file = new File(fileName);
+        try {
+            BufferedWriter out = new BufferedWriter(new FileWriter("Texts\\" + file));
+            out.write(content);
+            out.close();
+        } catch (IOException e) {
+            System.out.println("Blad odczytu pliku.");
+            System.exit(2);
+        }
+    }
+
+    public static ArrayList<File> getFiles() {
+        try {
+            File folder = new File("Texts");
+            ArrayList<File> files = new ArrayList<File>();
+
+            for (File file : folder.listFiles()) {
+                if (!file.isDirectory() && ObslugaPlikow.getFileExtension(file).equals("txt")) {
+                    // System.out.println(file.getName());
+                    // try {
+                    //     String fileText = new Scanner(file, "UTF-8").useDelimiter("\\A").next();
+                    //     System.out.println(fileText);
+                    // } catch (IOException e) {
+                    //     System.out.println("Blad odczytu pliku.");
+                    //     System.exit(2);
+                    // }
+                    files.add(file);
+                }
+            }
+            return files;
+        } catch (NullPointerException e) {
+            System.out.println("Nie ma katalogu (Texts).");
+        }
+        return null;
+    }
+}
