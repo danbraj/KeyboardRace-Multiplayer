3e3e187c97282767200c32be3ea36d0d40707f73
danbraj <danbraj@users.noreply.github.com>
2017-05-01 22:07:15 +0200

    rozwinięcie klasy Packet

--- Packet.java
+++ Packet.java
@@ -5,6 +5,7 @@ class Packet implements Serializable {
 
     Command command;
     String parameter;
+    int senderId = -1;
     
     public Packet(Command command) {
         this.command = command;
@@ -15,11 +16,34 @@ class Packet implements Serializable {
         this.parameter = parameter;
     }
 
+    public Packet(Command command, int senderId, String parameter) {
+        this.command = command;
+        this.senderId = senderId;
+        this.parameter = parameter;
+    }
+
     public Command getCommand() {
         return this.command;
     }
 
     public String getParameter() {
         return this.parameter;
+    }
+
+    public int getPlayerId() {
+        return this.senderId;
+    }
+}
+
+class PacketWithPlayersList extends Packet {
+    ArrayList<Player> players;
+
+    public PacketWithPlayersList(ArrayList<Player> players) {
+        super(Command.UPDATE_PLAYERS_LIST);
+        this.players = players;
+    }
+
+    public ArrayList<Player> getPlayers() {
+        return this.players;
     }
 }
