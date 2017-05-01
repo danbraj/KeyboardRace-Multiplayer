98c508e63c99026c01f6aca2a2148a1a078c572e
danbraj <danbraj@users.noreply.github.com>
2017-05-01 22:04:53 +0200

    dodanie klasy Player

---
+++ Player.java
@@ -0,0 +1,25 @@
+import java.io.Serializable;
+
+class Player implements Serializable {
+    int idPlayer = -1;
+    String nick = "---";
+    boolean isReady = false;
+    int numberOfWins = 0;
+    int actionPoints = 0;
+
+    public void setId(int id) {
+        this.idPlayer = id;
+    }
+
+    public int getId() {
+        return this.idPlayer;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+
+    public String getNick() {
+        return this.nick;
+    }
+}
